ee9ad86a015f8e6ee6b6f5e24613bea926a4f1de
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-19 19:41:09 -0400

    updated story_4

--- story_4.js
+++ story_4.js
@@ -1,20 +1,21 @@
-var story_4 = `</p>Geometry is an important part of the grade school curriculum. Bobby's lesson today is on calculating circumferences of shapes. 
-The math teacher, Mrs. Smith, taught Bobby and his classmates the formulas for rectangles, squares, triangles, circles, and trapezoids.</p>`
+var story_4 = `</p>Geometry is an important part of the grade school curriculum. Bobby's lesson today is on calculating circumferences of shapes. The math teacher, Mrs. Smith, taught Bobby and his classmates the formulas for rectangles, squares, triangles, circles, and trapezoids.</p>`
 
-var comp_questions = {
+var comp_questions_4 = {
   question_1: {
     question: "What formula did Bobby learn today?",
     A: "Circumference of triangle",
     B: "Circumference of hexagon",
     C: "Circumference of rhombus",
     D: "Not answerable",
+    correct_response: "A",
   },
   question_2: {
-    question: "What is the name of the shape whose circumefernce is 'pi-r-squared'?",
+    question: "What is the name of the shape whose circumference is 'pi-r-squared'?",
     A: "Circle",
     B: "Triangle",
     C: "Hexagon",
     D: "Not answerable",
+    correct_response: "A",
   },
   question_3: {
     question: "What is the name of Bobby's principal?",
@@ -22,6 +23,7 @@ var comp_questions = {
     B: "Mr. Richardson",
     C: "Mr. Smith",
     D: "Not answerable",
+    correct_response: "D",
   },
   question_4: {
     question: "Which formula did Bobby have a hard time learning?",
@@ -29,19 +31,30 @@ var comp_questions = {
     B: "Squares",
     C: "Triangles",
     D: "Not answerable",
+    correct_response: "D",
+  },
+  question_attn: {
+    question: "Which of the following is not a shape?",
+    A: "Squares",
+    B: "Light",
+    C: "Hearts",
+    D: "Not answerable",
+    correct_response: "B",
   },
   question_5: {
     question: "What is the name of Bobby's math teacher?",
     A: "Mrs. Smith",
     B: "Ms. Smith",
     C: "Mr. Smith",
-    D: "Not answerable"
+    D: "Not answerable",
+    correct_response: "A",
   },
   question_6:{
     question: "What school is Bobby in?",
     A: "High school",
     B: "Grade school",
     C: "Kindergarten",
-    D: "Not answerable"
-  }
+    D: "Not answerable",
+    correct_response: "B",
+  },
 };
