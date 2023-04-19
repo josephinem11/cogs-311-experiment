29d656ff6e66d9c0f53e48548eac81b7685d0637
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-19 19:44:48 -0400

    updated story_6

--- story_6.js
+++ story_6.js
@@ -1,13 +1,14 @@
 var story_6 = `</p>Kira is going to purchase a brand new house. Her HOA decided to paint all of the units the same color so the new residents can customize them how they see fit. 
-Kira wants her house to be simple: one-story, with no attic, and with no basement. She would like the house’s floor to be blue and the walls and ceiling are white.</p>`
+Kira wants her house to be simple: one-story, with no attic, and with no basement. She would like the house's floor to be blue and the walls and ceiling are white.</p>`
 
-var comp_questions = {
+var comp_questions_6 = {
   question_1: {
     question: "Who painted all the units white?",
     A: "Kira",
     B: "HOA",
     C: "HBO",
     D: "Not answerable",
+    correct_response: "B",
   },
   question_2: {
     question: "What color are the stairs?",
@@ -15,6 +16,7 @@ var comp_questions = {
     B: "White",
     C: "Wood",
     D: "Not answerable",
+    correct_response: "D",
   },
   question_3: {
     question: "Where is Kira's new home going to be?",
@@ -22,6 +24,7 @@ var comp_questions = {
     B: "Arkansas",
     C: "New Hampshire",
     D: "Not answerable",
+    correct_response: "D",
   },
   question_4: {
     question: "Who is painting Kira's new home?",
@@ -29,19 +32,30 @@ var comp_questions = {
     B: "Kira",
     C: "Kira's boyfriend",
     D: "Not answerable",
+    correct_response: "D",
+  },
+  question_attn: {
+    question: "Which of the following items can be found inside a living room?",
+    A: "A spaceship",
+    B: "A lake",
+    C: "A couch",
+    D: "Not answerable",
+    correct_response: "C",
   },
   question_5:{
     question: "What color does Kira want her ceiling to be?",
     A: "Blue",
     B: "White",
     C: "Wood",
-    D: "Not answerable"
+    D: "Not answerable",
+    correct_response: "B",
   },
   question_6: {
-    question: "Whhat does Kira not want in her new home?",
+    question: "What does Kira not want in her new home?",
     A: "Blue floor",
     B: "White ceiling",
     C: "Big attic",
-    D: "Not answerable"
-  }
+    D: "Not answerable",
+    correct_response: "C",
+  },
 };
