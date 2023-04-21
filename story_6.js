684a0e567528fb88b46f3a5e9ff37c3edfe4f8d8
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-21 02:38:39 -0400

    Add files via upload

---
+++ story_6.js
@@ -0,0 +1,61 @@
+var story_6 = `</p>Kira is going to purchase a brand new house. The housing company decided to paint all of the units the same color so the new residents can customize them how they see fit. 
+Kira wants her house to be simple: one-story, with no attic, and with no basement. She would like the house's floor to be blue and the walls and ceiling are white.</p>`
+
+var comp_questions_6 = {
+  question_1: {
+    question: "Who painted all the units white?",
+    A: "Kira",
+    B: "The construction company",
+    C: "The housing company",
+    D: "Not answerable",
+    correct_response: "C"
+  },
+  question_2: {
+    question: "What color are the stairs?",
+    A: "Blue",
+    B: "White",
+    C: "Wood",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_3: {
+    question: "Where is Kira's new home going to be?",
+    A: "Tennessee",
+    B: "Arkansas",
+    C: "New Hampshire",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_7: {
+    question: "Who is painting Kira's new home?",
+    A: "The housing company",
+    B: "Kira",
+    C: "Kira's boyfriend",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_4:{
+    question: "What color does Kira want her ceiling to be?",
+    A: "Blue",
+    B: "White",
+    C: "Wood",
+    D: "Not answerable",
+    correct_response: "B"
+  },
+  question_6: {
+    question: "What does Kira not want in her new home?",
+    A: "An attic",
+    B: "White ceiling",
+    C: "Blue floor",
+    D: "Not answerable",
+    correct_response: "A"
+  },
+  question_5:{
+    question: "Which of the following items can be found inside a living room?",
+    A: "A spaceship",
+    B: "A lake",
+    C: "A couch",
+    D: "Not answerable",
+    correct_response: "A couch"
+  },
+};
