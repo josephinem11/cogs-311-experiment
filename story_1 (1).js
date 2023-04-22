912a1ac4629468ddba3cf7681c254b45e16f060c
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-22 13:44:08 -0400

    Add files via upload

---
+++ story_1 (1).js
@@ -0,0 +1,63 @@
+  var story_1 = `<p><b>Uncle Jim owns a farm on the outskirts of Ireland. He mainly raises cows, roosters, and pigs on the farm. Cows and pigs are housed inside Uncle Jim's red barn while roosters like to spend their time on the barn roof. The roof is more slanted on the right side.</b></p>`;
+
+var story_1_inst = `<p style="text-align center;"><b>The recording has started. Please read the following passage out loud. Click 'Continue' when finished</b></p>
+<p style="text-align center;">Uncle Jim owns a farm on the outskirts of Ireland. He mainly raises cows, </p><p style="text-align center;">roosters, and pigs on the farm. Cows and pigs are housed inside Uncle Jim's red barn </p><p style="text-align center;">while roosters like to spend their time on the barn roof. The roof is more slanted on the right side.</p>`;
+
+var comp_questions_1 = {
+  question_1: {
+    question: "What animals were not on the farm?",
+    A: "Cows",
+    B: "Pigs",
+    C: "Sheep",
+    D: "Not answerable",
+    correct_response: "C",
+  },
+  question_4: {
+    question: "How many animals does Uncle Jim mainly raise?",
+    A: "1",
+    B: "2",
+    C: "3",
+    D: "Not answerable",
+    correct_response: "C",
+  },
+  question_3: {
+    question: "How many doors are there to Uncle Jim`s barn?",
+    A: "1",
+    B: "2",
+    C: "3",
+    D: "Not answerable",
+    correct_response: "D",
+  },
+  question_2: {
+    question: "If a rooster laid an egg on the apex of the slanted barn roof, which way would the egg roll, left or right?",
+    A: "Left",
+    B: "Right",
+    C: "Down",
+    D: "Not answerable",
+    correct_response: "D",
+  },
+  question_6: {
+    question: "Which side is the roof more slanted on?",
+    A: "The right side",
+    B: "The left side",
+    C: "The roof is not slanted",
+    D: "Not answerable",
+    correct_response: "A",
+  },
+  question_7: {
+    question: "Which part of Ireland does he live in?",
+    A: "North West",
+    B: "Northern Ireland",
+    C: "Midland East",
+    D: "Not answerable",
+    correct_response: "D",
+  },
+  question_5: {
+    question: "Which of the following is not an animal?",
+    A: "Pigs",
+    B: "Pineapple",
+    C: "Dolphin",
+    D: "Not answerable",
+    correct_response: "Pineapple",
+  },
+};
