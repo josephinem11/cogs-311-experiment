db6662b5c0a28b8692dbb2c09904c33548b7636d
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-22 13:47:15 -0400

    Add files via upload

---
+++ story_4 (1).js
@@ -0,0 +1,64 @@
+var story_4 = `<p>Geometry is an important part of the grade school curriculum. Bobby's lesson today is on calculating circumferences of shapes.
+The math teacher, Mrs. Smith, taught Bobby and his classmates the formulas for rectangles, squares, triangles, circles, and trapezoids.</p>`
+
+var story_4_inst = `<p style="text-align center;"><b>The recording has started. Please read the following passage out loud. Click 'Continue' when finished</b></p>
+<p style="text-align center;">Geometry is an important part of the grade school curriculum.</p><p style="text-align center;"> Bobby's lesson today is on calculating circumferences of shapes. The math teacher,</p><p style="text-align center;">Mrs. Smith, taught Bobby and his classmates the formulas for </p><p style="text-align center;">rectangles, squares, triangles, circles, and trapezoids.</p>`
+
+var comp_questions_4 = {
+  question_1: {
+    question: "What formula did Bobby learn today?",
+    A: "Circumference of triangle",
+    B: "Circumference of hexagon",
+    C: "Circumference of rhombus",
+    D: "Not answerable",
+ correct_response: "A" 
+  },
+  question_2: {
+    question: "What is the name of the shape whose circumefernce is 'pi-r-squared'?",
+    A: "Circle",
+    B: "Triangle",
+    C: "Hexagon",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_3: {
+    question: "What is the name of Bobby's principal?",
+    A: "Mrs. Smith",
+    B: "Mr. Richardson",
+    C: "Mr. Smith",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_7: {
+    question: "Which formula did Bobby have a hard time learning?",
+    A: "Rectangles",
+    B: "Squares",
+    C: "Triangles",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_4: {
+    question: "What is the name of Bobby's math teacher?",
+    A: "Mrs. Smith",
+    B: "Ms. Smith",
+    C: "Mr. Smith",
+    D: "Not answerable",
+    correct_response: "A"
+  },
+  question_6:{
+    question: "What school is Bobby in?",
+    A: "High school",
+    B: "Grade school",
+    C: "Kindergarten",
+    D: "Not answerable",
+    correct_response: "B"
+  },
+question_5:{
+   question: "Which of the following is not a shape?",
+    A: "Squares",
+    B: "Light",
+    C: "Hearts",
+    D: "Not answerable",
+    correct_response: "Light"
+},
+};
