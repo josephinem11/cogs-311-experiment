f0aad59081a8a470427f7e41095c57fd110d52ce
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-22 13:48:27 -0400

    Add files via upload

---
+++ story_5 (1).js
@@ -0,0 +1,63 @@
+var story_5 = `<p><b>The Sherpas, members of the Nepalese ethnic group, resided on the high plateaus of the Himalayan even before Mount Everest was discovered. Ballabh, a Sherpa boy born in the 1830s, was famous for his climbing ability. He climbed the highest mountain in 1849: Kangchenjunga Mountain.</b></p>`
+
+var story_5_inst = `<p style="text-align center;"><b>The recording has started. Please read the following passage out loud. Click 'Continue' when finished</b></p>
+<p style="text-align center;">The Sherpas, members of the Nepalese ethnic group, resided on </p><p style="text-align center;">the high plateaus of the Himalayan even before Mount Everest was discovered. </p><p style="text-align center;"> Ballabh, a Sherpa boy born in the 1830s, was famous for his</p><p style="text-align center;"> climbing ability. He climbed the highest mountain in 1849: Kangchenjunga Mountain.</p>`
+
+var comp_questions_5 = {
+  question_1: {
+    question: "What ethnic group is Ballabh from?",
+    A: "The Andamanese",
+    B: "The Hmong",
+    C: "The Sherpas",
+    D: "Not answerable",
+    correct_response: "C",
+  },
+  question_2: {
+    question: "What was the highest mountain in 1850?",
+    A: "Mount Andamanese",
+    B: "Kangchenjunga Mountain",
+    C: "Mount Everest",
+    D: "Not answerable",
+    correct_response: "C"
+  },
+  question_3: {
+    question: "What kind of houses do the Sherpas live in?",
+    A: "Hut",
+    B: "Igloo",
+    C: "Brick houses",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_4: {
+    question: "When was Ballabh born??",
+    A: "1800s",
+    B: "1890s",
+    C: "1830s",
+    D: "Not answerable",
+    correct_response: "C"
+  },
+  question_7: {
+    question: "When did Ballabh get into climbing?",
+    A: "1830",
+    B: "1834",
+    C: "1837",
+    D: "Not answerable",
+    correct_response: "D"
+  },
+  question_6:{
+    question: "What kind of environments do the sherpas live in?",
+    A: "Rich grassland",
+    B: "High plateau",
+    C: "Cold tundra",
+    D: "Not answerable",
+    correct_response: "B"
+  },
+  question_5: {
+    question: "Which of the following is a geographic landform?",
+    A: "Mountains",
+    B: "Libraries",
+    C: "Mansions",
+    D: "Not answerable",
+    correct_response: "Mountains"
+  },
+};
