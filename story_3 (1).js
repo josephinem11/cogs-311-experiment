0305399e01a4baba1706723ea6a3d6dcb079c19b
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-22 13:46:14 -0400

    Add files via upload

---
+++ story_3 (1).js
@@ -0,0 +1,63 @@
+var story_3 = `<p><b>Sarah is given an assignment for physics class. She must build a model bridge that can withstand at least twice its own weight using a variety of items of one pound each: toothpicks, rocks, popsicle sticks, steel, and hot glue. She plans to visit her friend, Hannah, next weekend in San Francisco to tour the Golden Gate Bridge and get some inspiration for her assignment.</b></p>`;
+
+var story_3_inst = `<p style="text-align center;"><b>The recording has started. Please read the following passage out loud. Click 'Continue' when finished</b></p>
+<p style="text-align center;">Sarah is given an assignment for physics class. She must build a model bridge that <p><p style="text-align center;">can withstand at least twice its own weight using a variety of items of one pound each:<p><p style="text-align center;"> toothpicks, rocks, popsicle sticks, steel, and hot glue. She plans to visit her friend, Hannah, <p><p style="text-align center;">next weekend in San Francisco to tour the Golden Gate Bridge and get some inspiration for her assignment.</p>`;
+
+var comp_questions_3 = {
+    question_3: {
+        question: "What color should Sarah`s bridge be?",
+        A: "Blue ",
+        B: "Frog green",
+        C: "Baby pink",
+        D: "Not answerable",
+        correct_response: "D",
+    },
+    question_1: {
+        question: "What class was this assignment for?",
+        A: "Math ",
+        B: "Physics",
+        C: "Chemistry ",
+        D: "Not answerable",
+        correct_response: "B",
+    },
+    question_4: {
+        question: "Where is Sarah going next weekend?",
+        A: "The TV",
+        B: "San Francisco",
+        C: "Toronto",
+        D: "Not answerable",
+        correct_response: "B",
+    },
+    question_6: {
+        question: "What material was not available for Sarah to use?",
+        A: "Steel",
+        B: "Hot glue",
+        C: "Toothpicks",
+        D: "Not answerable",
+        correct_response: "C",
+    },
+    question_7: {
+        question: "How many toothpicks would be needed to build a bridge that can support 10 lbs?",
+        A: "100,000",
+        B: "500,000",
+        C: "200",
+        D: "Not answerable",
+        correct_response: "D",
+    },
+    question_2: {
+        question: "Which is heavier? The rocks, or the popsicle sticks?",
+        A: "Rocks",
+        B: "Popsicle sticks",
+        C: "Steel",
+        D: "Not answerable",
+        correct_response: "D",
+    },
+    question_5: {
+        question: "Which of the following items are white and round?",
+        A: "Popsicle sticks",
+        B: "Toothpicks",
+        C: "Cotton balls",
+        D: "Not answerable",
+        correct_response: "Cotton balls",
+    },
+};
