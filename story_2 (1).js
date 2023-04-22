64e78551ee62722aea97a3aec5be8cb79e2ace8f
josephinem11 <42146442+josephinem11@users.noreply.github.com>
2023-04-22 13:45:17 -0400

    Add files via upload

---
+++ story_2 (1).js
@@ -0,0 +1,63 @@
+var story_2 = `<p><b>Jake, who lives in California, is an avid hiker who likes to explore the outdoors. On one of his walks, he ran into his widow's sister, Aimee, who he had briefly met before in her Santa Monica home. The two hikers started chatting. It turned out they had a lot in common and enjoyed each other's company. After that day, they started spending a lot of time together and eventually decided they have feelings for each other and want to have a future together. When the time arrived for marriage to be discussed, moral and legal boundaries were put into question. The new law passed that residents of California cannot marry their spouse's siblings.</b></p>`;
+
+var story_2_inst = `<p style="text-align center;"><b>The recording has started. Please read the following passage out loud. Click 'Continue' when finished</b></p>
+<p style="text-align center;">Jake, who lives in California, is an avid hiker who likes to explore the outdoors. </p><p style="text-align center;">On one of his walks, he ran into his widow's sister, Aimee, who he had </p><p style="text-align center;"> briefly met before in her Santa Monica home. The two hikers started chatting.</p><p style="text-align center;">It turned out they had a lot in common and enjoyed each other's company.</p><p style="text-align center;"> After that day, they started spending a lot of time together and eventually</p><p style="text-align center;"> decided they have feelings for each other and want to have a future together.</p><p style="text-align center;"> When the time arrived for marriage to be discussed, moral and legal boundaries</p><p style="text-align center;"> were put into question. The new law passed that residents of California </p><p style="text-align center;"> cannot marry their spouse's siblings.</p>`;
+
+var comp_questions_2 = {
+  question_2: {
+    question: "Is it legal for a man in California to marry his widow`s sister?",
+    A: "Yes",
+    B: "Yes but they cannot be California residents",
+    C: "No",
+    D: "Not answerable",
+    correct_response: "D",
+  },
+  question_1: {
+    question: "Had Jake and Aimee met before?",
+    A: "Yes, briefly",
+    B: "Yes, they knew each other for a long time",
+    C: "No",
+    D: "Not answerable",
+    correct_response: "A",
+  },
+  question_3: {
+    question: "What ignited Jake and Aimee`s passion for cooking?",
+    A: "Cooking class",
+    B: "They had wonderful pasta when going out to a restaurant",
+    C: "Their new kitchen utensi",
+    D: "Not answerable",
+    correct_response: "D",
+  },
+  question_7: {
+    question: "When was the new law passed?",
+    A: "2020",
+    B: "2014",
+    C: "2013",
+    D: "Not answerable",
+    correct_response: "D",
+  },
+  question_4: {
+    question: "Does Aimee have a sister? ",
+    A: "Yes",
+    B: "No",
+    C: "Jake has a sister",
+    D: "Not answerable",
+    correct_response: "A",
+  },
+  question_6: {
+    question: "Where does Aimme live?",
+    A: "New York City",
+    B: "Nashville",
+    C: "Santa Monica",
+    D: "Not answerable",
+    correct_response: "C",
+  },
+  question_5: {
+    question: "Which of the following is a state that begins with the same first letter as California?",
+    A: "Colorado",
+    B: "Nebraska",
+    C: "Nebraska",
+    D: "Not answerable",
+    correct_response: "A",
+  },
+};
